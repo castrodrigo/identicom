57fbdbc0be6010d4398acef2cd7267afabf5a9af
castrodrigo <castrodrigo@yahoo.com.br>
2019-08-14 22:33:28 +0200

    Test fiter_odd_squares

--- test/identicon_test.exs
+++ test/identicon_test.exs
@@ -18,5 +18,13 @@ defmodule IdenticonTest do
 
     assert Identicon.mirrow_row(list_row) == [1, 2, 3, 2, 1]
   end
+
+  test "filter_odd_squares should return just tuples where numbers are even" do
+    list_row_with_tuples = [{11, 1}, {21, 2}, {32, 3}, {44, 4}, {59, 5}]
+
+    %Identicon.Image{grid: output} =
+      Identicon.filter_odd_squares(%Identicon.Image{grid: list_row_with_tuples})
+
+    assert output == [{32, 3}, {44, 4}]
   end
 end
