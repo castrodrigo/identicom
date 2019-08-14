c07686004b71a350c99ad5022f0499f22c2a55ca
castrodrigo <castrodrigo@yahoo.com.br>
2019-08-14 22:32:48 +0200

    Test hash_input

--- test/identicon_test.exs
+++ test/identicon_test.exs
@@ -2,7 +2,15 @@ defmodule IdenticonTest do
   use ExUnit.Case
   doctest Identicon
 
-  test "greets the world" do
-    assert Identicon.hello() == :world
+  test "hash_input always returns the same list with 16 items for each String" do
+    input = "This is a test"
+    %Identicon.Image{hex: hex} = Identicon.hash_input(input)
+
+    assert length(hex) == 16
+
+    %Identicon.Image{hex: hex2} = Identicon.hash_input(input)
+
+    assert hex == hex2
+  end
   end
 end
